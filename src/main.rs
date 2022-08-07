41437eb3ea2c37c94d701cb35e80e0922755df5f
Shroomerian <shroomerian@protonmail.com>
2022-08-07 19:51:40 +0200

    Optimized the sort by ordering the left values first, also added comments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 //The structure implements an Option in the case that
 //the total amount of numbers is uneven.
 
+//Tuple sort works by separating the numbers into pairs of two called nodes, this allows
+//to order the two numbers within the node extremely fast since it's a 1:1 comparison
+
+//After the given vector of numbers has been given to the function, it will divide them into nodes using chunks
+//and it will proceed to sort them firstly by sorting the lowest numbers and setting them in position
+//and then comparing the right most numbers against the neighbour's left most number to see which one is smaller
+//and changes them accordingly until fully solved.
+
+#[derive(Copy, Clone)]
 struct Node(u32,Option<u32>);
 
 impl Node {
@@ -28,11 +37,18 @@ impl Node {
     }
 }
 
+/*
+fn sort_loop(left: &u32, right: &u32) {
+    
+}
+*/
+
+//Sorts a vector of numbers
 fn tuple_sort(list: &mut [u32]) -> Vec<Vec<u32>> {
     let mut sorted_vector = Vec::new();
 
+    //Creates nodes from chunks of the vector
     for chunk in list.chunks(2) {
-
         let mut node: Node;
 
         if chunk.len() == 2 {
@@ -46,13 +62,48 @@ fn tuple_sort(list: &mut [u32]) -> Vec<Vec<u32>> {
         sorted_vector.push(node);
     }
 
+    //Mutable values used to control the while loop
     let mut sorted = false;
     let mut counter = 0;
     let mut reset = 0;
+
+    //Initial ordering of the nodes by their leftmost values
+    while sorted == false {
+        if sorted_vector[counter].0 > sorted_vector[counter+1].0 {
+            let temp = sorted_vector[counter];
+            sorted_vector[counter] = sorted_vector[counter+1];
+            sorted_vector[counter+1] = temp;
+        }
+
+        //Increment counter
+        counter += 1;
+        
+        //Reset if to avoid accessing out of bounds
+        if counter == sorted_vector.len() - 1 { // -1 because the if above adds +1 to the counter
+        
+            if reset >= sorted_vector.len() {
+                //Closing boolean
+                counter = 0;
+                reset = 0;
+                sorted = true;
+            }
+        
+            //Resets counter and increments the reset variable
+            counter = 0;
+            reset += 1;
+        }
+    }
+
+    //Reset of the previous values
+    sorted = false;
+
+    //Info on how many times the while loop ran, including resets.
     let mut total = 0;
 
+    //Final sort of the values by comparing right and left values of neighbouring nodes
     while sorted == false  {
-
+        
+        //Guard if if the stored value is None.
         if let Some(number) = sorted_vector[counter].1 {
 
             if number > sorted_vector[counter+1].0 {
@@ -66,17 +117,23 @@ fn tuple_sort(list: &mut [u32]) -> Vec<Vec<u32>> {
             } 
         }
 
+        //Increment counter
         counter += 1;
+
+        //Info
         total += 1;
 
-        if counter == sorted_vector.len() - 1 {
+        //Reset if to avoid accessing out of bounds
+        if counter == sorted_vector.len() - 1 { // -1 because the if above adds +1 to the counter
 
-            if reset >= sorted_vector.len() {
+            if reset == 1 {
+                //Info dump
                 println!("Total reads done: {}",total);
-                println!("Total resets done: {}",reset);
+                //Closing boolean
                 sorted = true;
             }
 
+            //Resets counter and increments the reset variable
             counter = 0;
             reset += 1;
         }
